ae702ce42442c9d6809d9b3c92e8ed58614c5f64
l <eh.chahkiev@smartics.ru>
2025-08-21 20:40:13 +0300

    feat(config): add .env file

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule Phonix.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:dotenvy, "~> 1.1.0"},
       {:phoenix, "~> 1.8.0"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
