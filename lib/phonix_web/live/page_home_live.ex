0f5b5dd05c65891fe0da48abad697aec8803e6c1
l <eh.chahkiev@smartics.ru>
2025-08-21 23:25:13 +0300

    feat: create accounts

--- lib/phonix_web/live/page_home_live.ex
+++ lib/phonix_web/live/page_home_live.ex
@@ -7,21 +7,6 @@ defmodule PhonixWeb.LivePageHome do
 
   def render(assigns) do
     ~H"""
-      <div>
-        <.input name="test" type="text" value="" />
-      </div>
-
-
-      <.list>
-        <:item title="Title">test</:item>
-        <:item title="Views">test2</:item>
-      </.list>
-
-
-      <.button>Send!</.button>
-      <.button phx-click="go" variant="primary">Send!</.button>
-      <.button navigate={~p"/"}>Home</.button>
-      <.flash kind={:info} phx-mounted={show("#flash")}>testme</.flash>
     """
   end
 
