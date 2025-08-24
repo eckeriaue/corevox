55183245cac1aa3bf8faee81d568ba0636d3413e
l <eh.chahkiev@smartics.ru>
2025-08-24 19:26:44 +0300

    feat!: add auth

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,7 @@
 import Config
+
+# Only in tests, remove the complexity from the password hashing algorithm
+config :bcrypt_elixir, :log_rounds, 1
 # import Dotenv
 
 # Dotenv.load()
--- lib/phonix_web/router.ex
+++ lib/phonix_web/router.ex
@@ -1,6 +1,8 @@
 defmodule PhonixWeb.Router do
   use PhonixWeb, :router
 
+  import PhonixWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule PhonixWeb.Router do
     plug :put_root_layout, html: {PhonixWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -41,5 +44,33 @@ defmodule PhonixWeb.Router do
       live_dashboard "/dashboard", metrics: PhonixWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", PhonixWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{PhonixWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", PhonixWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{PhonixWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
