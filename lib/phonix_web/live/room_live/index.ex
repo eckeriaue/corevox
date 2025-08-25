fa6c5292983a9565857d83ec0d4f6d2a36392f7f
l <eh.chahkiev@smartics.ru>
2025-08-25 18:15:38 +0300

    feat: validaiton rooms

--- lib/phonix_web/live/room_live/index.ex
+++ lib/phonix_web/live/room_live/index.ex
@@ -12,12 +12,18 @@ defmodule PhonixWeb.RoomLive.Index do
     <Layouts.app flash={@flash} current_scope={@current_scope}>
       <div class="flex justify-between">
         <.header>
-        Список доступных комнат
+          Список доступных комнат
         </.header>
         <.button href={~p"/rooms/new"} class="btn btn-primary">
           Создать <span aria-hidden="true">&rarr;</span>
         </.button>
       </div>
+
+      <%= if Enum.empty?(@rooms) do %>
+        <p>Доступных комнат пока нет</p>
+      <%= else %>
+        <p>доступные комнаты есть, но я их не покажу</p>
+      <% end %>
     </Layouts.app>
     """
   end
