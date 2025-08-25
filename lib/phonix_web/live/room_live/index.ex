d64be4e8bbe2af1ee2cb1a17e36b533be561fc13
l <eh.chahkiev@smartics.ru>
2025-08-25 17:56:58 +0300

    feat: index heading

--- lib/phonix_web/live/room_live/index.ex
+++ lib/phonix_web/live/room_live/index.ex
@@ -10,7 +10,14 @@ defmodule PhonixWeb.RoomLive.Index do
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash} current_scope={@current_scope}>
-      test me
+      <div class="flex justify-between">
+        <.header>
+        Список доступных комнат
+        </.header>
+        <.button href={~p"/rooms/new"} class="btn btn-primary">
+          Создать <span aria-hidden="true">&rarr;</span>
+        </.button>
+      </div>
     </Layouts.app>
     """
   end
