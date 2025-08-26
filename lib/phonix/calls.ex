f09a8f27f73d306e8fa705ac067dbbeb61ceb308
l <eh.chahkiev@smartics.ru>
2025-08-26 22:24:30 +0300

    feat: validate registered user

--- lib/phonix/calls.ex
+++ lib/phonix/calls.ex
@@ -2,6 +2,7 @@ defmodule Phonix.Calls do
   import Ecto.Query, warn: false
   alias Phonix.Repo
   alias Phonix.Calls.Room
+  alias Phonix.Accounts.User
   alias Phonix.Calls.RoomMember
 
   def list_rooms do
@@ -9,6 +10,14 @@ defmodule Phonix.Calls do
   end
 
   def get_room!(id), do: Room |> Repo.get!(id)
+
+  def get_room_members(id) do
+    query = from room_members in RoomMember,
+      join: user in User, on: user.id == room_members.user_id,
+      where: room_members.room_id == ^id,
+      select: user
+    Repo.all(query)
+  end
 
   def change_room(room, attrs \\ %{}) do
     Room.changeset(room, attrs)
--- lib/phonix_web/live/room_live/show.ex
+++ lib/phonix_web/live/room_live/show.ex
@@ -16,21 +16,37 @@ defmodule PhonixWeb.RoomLive.Show do
 
   @impl true
   def mount(params, _session, socket) do
-    id = params["id"]
 
-    IO.inspect(Calls.join_room(socket.assigns.current_scope.user, String.to_integer(id)))
-    case Calls.join_room(socket.assigns.current_scope.user, String.to_integer(id)) do
-      {:error, _} ->
-        {:ok,
-          socket
-            |> put_flash(:error, "При входе в комнату произошла ошибка")
-            |> redirect(to: ~p"/rooms/#{id}/prepare")
-          }
-      {:ok, room_member} ->
-        {:ok,
-          socket
-            |> assign(:room_id, id)
+    room_id = params["id"]
+    user = get_in(socket.assigns, [:current_scope, :user])
+
+    # with nil <- user do
+
+    # end
+
+    if user == nil do
+      {:ok,
+        socket
+          |> put_flash(:info, "Чтобы войти в комнату вы должны быть зарегестрированы")
+          |> redirect(to: ~p"/users/register")
         }
+    else
+      members = Calls.get_room_members(String.to_integer(room_id)) |> Enum.reject(fn member -> member.id == user.id end)
+
+      case user |> Calls.join_room(String.to_integer(room_id)) do
+        {:error, _} ->
+          {:ok,
+            socket
+              |> put_flash(:error, "При входе в комнату произошла ошибка")
+              |> redirect(to: ~p"/rooms/#{room_id}/prepare")
+            }
+        {:ok, room_member} ->
+          {:ok,
+            socket
+              |> assign(:room_id, room_id)
+              |> assign(:room_member, room_member)
+          }
+      end
     end
   end
 end
