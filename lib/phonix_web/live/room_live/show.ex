66554be2406ce44cca04c772238775275341cdd2
l <eh.chahkiev@smartics.ru>
2025-08-26 22:43:54 +0300

    feat: use case sintax

--- lib/phonix_web/live/room_live/show.ex
+++ lib/phonix_web/live/room_live/show.ex
@@ -17,36 +17,34 @@ defmodule PhonixWeb.RoomLive.Show do
   @impl true
   def mount(params, _session, socket) do
 
-    room_id = params["id"]
-    user = get_in(socket.assigns, [:current_scope, :user])
+    room_id = String.to_integer(params["id"])
 
-    # with nil <- user do
+    case user = get_in(socket.assigns, [:current_scope, :user]) do
+      nil ->
+        {:ok,
+          socket
+            |> put_flash(:info, "Чтобы войти в комнату вы должны быть зарегестрированы")
+            |> redirect(to: ~p"/users/register")}
 
-    # end
+      %{} ->
+        members = Calls.get_room_members(room_id)
+          |> Enum.reject(fn member -> member.id == user.id end)
 
-    if user == nil do
-      {:ok,
-        socket
-          |> put_flash(:info, "Чтобы войти в комнату вы должны быть зарегестрированы")
-          |> redirect(to: ~p"/users/register")
-        }
-    else
-      members = Calls.get_room_members(String.to_integer(room_id)) |> Enum.reject(fn member -> member.id == user.id end)
+        case Calls.join_room(user, room_id) do
+          {:error, _} ->
+            {:ok,
+              socket
+                |> put_flash(:error, "При входе в комнату произошла ошибка")
+                |> redirect(to: ~p"/rooms/#{room_id}/prepare")}
 
-      case user |> Calls.join_room(String.to_integer(room_id)) do
-        {:error, _} ->
-          {:ok,
-            socket
-              |> put_flash(:error, "При входе в комнату произошла ошибка")
-              |> redirect(to: ~p"/rooms/#{room_id}/prepare")
-            }
-        {:ok, room_member} ->
-          {:ok,
-            socket
-              |> assign(:room_id, room_id)
-              |> assign(:room_member, room_member)
-          }
-      end
+          {:ok, room_member} ->
+            {:ok,
+              socket
+                |> assign(:room_id, room_id)
+                |> assign(:room_member, room_member)
+                |> assign(:members, members)}
+
+        end
     end
   end
 end
