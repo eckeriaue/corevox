4b2c923cc68b4851c050d4edcf88727a1fddceba
l <eh.chahkiev@smartics.ru>
2025-09-03 18:07:31 +0300

    fix: docker postgres connect

--- app3/lib/corevox/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule CoreVox.Mailer do
-  use Swoosh.Mailer, otp_app: :corevox
-end
--- app3/lib/corevox/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule CoreVox.Repo do
-  use Ecto.Repo,
-    otp_app: :corevox,
-    adapter: Ecto.Adapters.Postgres
-end
