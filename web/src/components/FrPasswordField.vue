c9dd7471dc6faadf93ec732049c9a88033465e05
l <eh.chahkiev@smartics.ru>
2025-09-04 22:50:54 +0300

    feat(client): better validate

--- web/src/components/FrPasswordField.vue
+++ web/src/components/FrPasswordField.vue
@@ -1,30 +1,51 @@
 
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
+import { computed, ref } from 'vue'
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   name?: string
   label?: string
   errorMessages?: string[]
   placeholder?: string
   class?: HTMLAttributes['class']
-}>()
+}>(), {
+  errorMessages: () => []
+})
 
 
 const modelValue = defineModel<string>({ default: '' })
+const hasErrors = computed(() => props.errorMessages.length > 0)
+const canSee = ref(false)
 
 </script>
 <template>
     <fieldset class="fieldset group" :class="props.class">
     <legend class="fieldset-legend">{{ label }}</legend>
-    <input
-        type="password"
-        class="input w-full"
-        :placeholder
-        v-model="modelValue"
-    />
+    <label
+        class="w-full input"
+        :class="{
+          'input-error': hasErrors && modelValue.length > 0,
+        }"
+    >
+        <input
+            :type="canSee ? 'text' : 'password'"
+            class="w-full"
+            :placeholder
+            v-model="modelValue"
+        />
+        <button
+            type="button"
+            class="not-[&:hover]:opacity-80 block ph"
+            @click="canSee = !canSee"
+            :class="{
+              'ph-eye': canSee,
+              'ph-eye-slash': !canSee,
+            }"
+        />
+    </label>
     <p
-        class="label text-color-error group-focus-within:hidden"
+        class="label text-error group-focus-within:hidden"
         :class="{ 'hidden': modelValue.length === 0 }"
         v-for="error in errorMessages">{{ error }}</p>
     </fieldset>
--- web/src/components/FrTextField.vue
+++ web/src/components/FrTextField.vue
@@ -1,17 +1,20 @@
 
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
+import { computed, type HTMLAttributes } from 'vue'
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   name?: string
   label?: string
   errorMessages?: string[]
   placeholder?: string
   class?: HTMLAttributes['class']
-}>()
+}>(), {
+  errorMessages: () => []
+})
 
 
 const modelValue = defineModel<string>({ default: '' })
+const hasErrors = computed(() => props.errorMessages.length > 0)
 
 </script>
 <template>
@@ -21,10 +24,13 @@ const modelValue = defineModel<string>({ default: '' })
         type="text"
         class="input w-full"
         :placeholder
+        :class="{
+          'input-error': hasErrors && modelValue.length > 0,
+        }"
         v-model="modelValue"
     />
     <p
-        class="label text-color-error group-focus-within:hidden"
+        class="label text-error group-focus-within:hidden"
         :class="{ 'hidden': modelValue.length === 0 }"
         v-for="error in errorMessages">{{ error }}</p>
     </fieldset>
