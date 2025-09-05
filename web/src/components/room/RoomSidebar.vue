fa56ac90bb0534cba46c2647cefa6d157a029b34
l <eh.chahkiev@smartics.ru>
2025-09-05 23:12:02 +0300

    feat(room): sidebar

---
+++ web/src/components/room/RoomSidebar.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+
+const items = [
+  {
+    title: "Home",
+  },
+  {
+    title: "Inbox",
+  },
+  {
+    title: "Calendar",
+  },
+  {
+    title: "Search",
+  },
+  {
+    title: "Settings",
+  },
+];
+</script>
+
+<template>
+<SidebarProvider>
+  <Sidebar>
+    <SidebarContent>
+        <div class="flex items-center px-2 h-16">
+           <a href="/"  class="btn btn-ghost text-xl">CoreVox</a>
+       </div>
+      <SidebarGroup>
+        <SidebarGroupContent>
+          <SidebarMenu>
+              <SidebarMenuItem v-for="item in items" :key="item.title">
+                <SidebarMenuButton asChild>
+                    <span>{{item.title}}</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </SidebarContent>
+  </Sidebar>
+  <div class="flex flex-col border-r border-base-300 justify-between">
+      <ul>
+          <li>
+              <SidebarTrigger />
+          </li>
+      </ul>
+  </div>
+  <slot />
+</SidebarProvider>
+</template>
---
+++ web/src/components/ui/sidebar/SidebarTrigger.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue"
+import { PanelLeft } from "lucide-vue-next"
+import { cn } from "@/lib/utils"
+import { Button } from '@/components/ui/button'
+import { useSidebar } from "./utils"
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"]
+}>()
+
+const { toggleSidebar, state } = useSidebar()
+</script>
+
+<template>
+<button
+    type="button"
+    class="btn btn-ghost btn-square"
+    data-sidebar="trigger"
+    data-slot="sidebar-trigger"
+    @click="toggleSidebar"
+>
+    <span class="ph ph-sidebar" />
+</button>
+</template>
