edc9e9843d669965aa0247a0585cb426d814f295
l <eh.chahkiev@smartics.ru>
2025-09-06 19:39:51 +0300

    chore(media): use 16/10

--- web/src/components/room/MyMedia.vue
+++ web/src/components/room/MyMedia.vue
@@ -71,7 +71,7 @@ onUnmounted(() => {
           'ring-primary': enableMicrophone,
         }"
         class="relative ring-2 bg-base-300 rounded-2xl flex items-center justify-center overflow-hidden"
-        style="aspect-ratio: 16 / 9; object-fit: cover; object-position: center;"
+        style="aspect-ratio: 16 / 10; object-fit: cover; object-position: center;"
     >
         <div class="absolute inset-2 size-[calc(100%_-_var(--spacing)*4)]">
             <media-short-info
