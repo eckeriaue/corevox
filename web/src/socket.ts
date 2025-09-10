684d91f03b381864c56465a4bbb47bcbdf8a519c
l <eh.chahkiev@smartics.ru>
2025-09-11 02:14:24 +0300

    fix: socket url

--- web/src/socket.ts
+++ web/src/socket.ts
@@ -1,6 +1,6 @@
 import { Socket } from 'phoenix'
 
-export const socket = typeof window !== 'undefined' ? new Socket(import.meta.env.PUBLIC_APP_SOCKET_URL) : null
+export const socket = typeof window !== 'undefined' ? new Socket('/socket') : null
 if (socket) {
   socket.connect()
 }
