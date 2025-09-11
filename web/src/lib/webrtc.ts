841e3125ffa454175f770732bc843da981d4af6b
l <eh.chahkiev@smartics.ru>
2025-09-11 17:15:54 +0300

    fix: turn server auth

--- web/src/lib/webrtc.ts
+++ web/src/lib/webrtc.ts
@@ -1,16 +1,17 @@
 const config = {
   iceServers: [
+    { urls: import.meta.env.PUBLIC_STUN_SERVER_URL },
+    // { urls: 'stun:stun.l.google.com:19302' },
+    // { urls: 'stun:stun1.l.google.com:19302' },
     {
-      urls: [import.meta.env.PUBLIC_STUN_SERVER_URL]
-    },
-    {
-      urls: "stun:stun.l.google.com:19302"
-    },
-    {
-      urls: [import.meta.env.PUBLIC_TURN_SERVER_URL],
+      urls: [
+        import.meta.env.PUBLIC_TURN_SERVER_URL,
+        import.meta.env.PUBLIC_TURN_SERVER_URL + '?transport=udp',
+        import.meta.env.PUBLIC_TURN_SERVER_URL + '?transport=tcp',
+      ],
       username: import.meta.env.PUBLIC_TURN_USER,
       credential: import.meta.env.PUBLIC_TURN_PASSWORD
-    }
+    },
   ],
 }
 
