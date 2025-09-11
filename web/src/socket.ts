a1e47feecddd70750b97f38467ce1d0428b5439b
l <eh.chahkiev@smartics.ru>
2025-09-11 03:07:44 +0300

    fix: add .env volume

--- web/src/socket.ts
+++ web/src/socket.ts
@@ -1,7 +1,7 @@
 import { Socket } from 'phoenix'
 
-console.info("import.meta.env", import.meta.env)
-export const socket = new Socket(import.meta.env.PUBLIC_APP_SOCKET_URL)
+console.info("import.meta.env", import.meta.env, globalThis.env)
+export const socket = new Socket(globalThis?.env?.PUBLIC_APP_SOCKET_URL || import.meta.env.PUBLIC_APP_SOCKET_URL)
 if (socket) {
   socket.connect()
 }
